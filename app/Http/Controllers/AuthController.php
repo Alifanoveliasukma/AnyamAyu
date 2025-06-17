adc7c04fab61facf026963660ee44cf28a6b10a7
Alifanoveliasukma <alifasukma11@gmail.com>
2025-06-17 20:35:32 +0700

    feature: multiple role, validation message

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Hash;
+use Str;
 
 class AuthController extends Controller
 {
@@ -11,6 +14,27 @@ class AuthController extends Controller
         $data['meta_title'] = 'Registration';
         return view('auth.registration');
     }
+
+    public function registration_post(Request $request)
+    {
+        $user = request()->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required|min:6',
+            'confirm_password' => 'required_with:password|same:password|min:6',
+            'is_role' => 'required',
+        ]);
+
+        $user = new User;
+        $user->name = trim($request->name);
+        $user->email = trim($request->email);
+        $user->password = Hash::make($request->password);
+        $user->is_role = trim($request->is_role);
+        $user->remember_token = Str::random(50);
+        $user->save();
+        return redirect('login')->with('success', 'Registration successfuly.');
+    }
+
     public function login()
     {
         $data['meta_title'] = 'Login';
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 // Registration
 Route::get('/registration', [AuthController::class, 'registration'])->name('auth.registration');
+Route::post('registration_post', [AuthController::class, 'registration_post'])->name('auth.registration.post');
 
 Route::get('login', [AuthController::class, 'login'])->name('auth.login');
 
