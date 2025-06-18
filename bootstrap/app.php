a5d1ec4e87e60b86c5040bd5067f4577615bf43f
Alifanoveliasukma <alifasukma11@gmail.com>
2025-06-18 14:19:16 +0700

    feat: setting middleware with multiple roles

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,11 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use App\Http\Middleware\SuperAdminMiddleware;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\UserMiddleware;
+
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,7 +16,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        
+        $middleware->alias([
+            'superadmin' => SuperAdminMiddleware::class,
+            'admin' => AdminMiddleware::class,
+            'user' => UserMiddleware::class
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
