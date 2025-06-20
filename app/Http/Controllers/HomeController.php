ffb988cfbb84d969642191212ddb686db26e85c4
Alifanoveliasukma <alifasukma11@gmail.com>
2025-06-20 14:11:23 +0700

    feat: auth check

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,6 @@ class HomeController extends Controller
     public function index()
     {
         $data['meta_title'] = 'Welcome';
-        return view('layouts.index', $data);
+        return view('index', $data);
     }
 }
