a48005f2f64c52632aed7c48880c55ec28941ed6
Alifanoveliasukma <alifasukma11@gmail.com>
2025-06-20 13:23:24 +0700

    feat: redirect to different views based on user role

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,16 +11,22 @@ class DashboardController extends Controller
    public function dashboard()
    {
 
-    if(Auth::user()->is_role == 2)
-    {
-        echo "S";die();
-    }else if(Auth::user()->is_role == 1)
-    {
-        echo "A";die();
-    }else if(Auth::user()->is_role == 0)
-    {
-        echo "U";die();
-    }
+        if(Auth::user()->is_role == 2)
+        {
+            $data['meta_title'] = "super admin dashboard page";
+            $data['getRecord'] = User::find(Auth::user()->id);
+            return view('superadmin.dashboard', $data);
+        }else if(Auth::user()->is_role == 1)
+        {
+            $data['meta_title'] = "Admin dashboard page";
+            $data['getRecord'] = User::find(Auth::user()->id);
+            return view('admin.dashboard', $data);
+        }else if(Auth::user()->is_role == 0)
+        {
+            $data['meta_title'] = "User dashboard page";
+            $data['getRecord'] = User::find(Auth::user()->id);
+            return view('user.dashboard', $data);
+        }
 
-   }
+    }
 }
